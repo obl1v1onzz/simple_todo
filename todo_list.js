a23c28fdf9a01e3bde4c4a82607e23d312d648e7
obl1v1onzz <oblivion16041604@gmail.com>
2021-11-06 14:45:06 +0300

    todo_completed

--- todo_list.js
+++ todo_list.js
@@ -1,7 +1,7 @@
-const statusTask = {
-  inProgress:'In Progress',
-  toDo: 'Todo',
-  done: 'Done'
+const ALL_STATUS_TASKS = { // Объект со статусом тасков. Можно дополнять итд.
+  inProgress:'В прогрессе', 
+  toDo: 'Todo', 
+  done: 'Done', 
 }
 
 const todoList = {
@@ -9,39 +9,46 @@ const todoList = {
 };
 
 let changeStatus = (task, newStatusTask) => {
-  if (todoList[task]) {
-    for(let keys in statusTask){
-      value = statusTask[keys]
-      if(value===newStatusTask){
+  if (todoList[task]) { // Проверяем если есть таск в листе.
+    for(let keys in ALL_STATUS_TASKS){
+      value = ALL_STATUS_TASKS[keys]
+      if(value===newStatusTask){ // Есть ли статус в листе статусов.
         todoList[task] = newStatusTask
       }
     }
-  } else {
-    console.log("undefined task");
-  }
+  } 
 };
 
 let addTask = (task)=>{
-  todoList[task] = statusTask.inProgress
+  todoList[task] = ALL_STATUS_TASKS.toDo 
 }
 let deleteTask = (task)=>{
-  todoList[task] ? delete todoList[task] : console.log('undefined task')
+  todoList[task] ? delete todoList[task] :  console.log('undefined task') // Проверяем есть ли таск в листе.
 }
 let printTodoList = () => {
-  let resultObj = {
-    'Todo' : '',
-    'In Progress' : '',
-    'Done' : '',
-  };
-  for(let key in todoList){
-    let value = todoList[key];
-    resultObj[value] = {...resultObj[value], [key]:''}
+  let resultObj=Object.assign({},ALL_STATUS_TASKS) // Копия объекта статусов тасков, в итоговой обьект для выввода
+  for(let x in resultObj){ // Нужно для заполнения объекта в следующем цикле
+    let value = resultObj[x]
+    delete resultObj[x]
+    resultObj[value] = '' 
+  }
+  for(let key in todoList){ 
+    resultObj[todoList[key]] = {...resultObj[todoList[key]], [key]:''} // Добавлям в итогой объект таски {'In Proggress': {TASK1:'', TASK2:''}, 'To Do': {task3:''}}
+  }
+  for(let key in resultObj){ // Вывод итоговый объект
+    console.log(key+':')
+    if(resultObj[key] == ''){ //Если нет задач 
+      console.log('    -')
     }
-    for(let ks in resultObj){
-      console.log(`${ks}:`)
-      for(let x in resultObj[ks]){
-        console.log(`    ${x}`)
-      }
-      
+    for(let j in resultObj[key]){
+      console.log('    '+ j)
     }
   }
+  }
+
+changeStatus('create a task','Done')
+addTask('Buy milk')
+addTask('Design a prototype')
+changeStatus('Buy milk', 'In Progress')
+addTask('Read the boo')
+printTodoList()
